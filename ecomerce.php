6e8edaab411120d03569e395fc5b457fd60be903
Ricardo Henrique Tavora Niess Brancaglion <ricardo3845@gmail.com>
2013-09-05 20:42:02 -0300

    XML file with product info created. PHP page loading that XML and displaying the product info.

---
+++ ecomerce.php
@@ -0,0 +1,47 @@
+<?php //ecomerce.php
+
+$xml= simplexml_load_file("/Users/Ricardo/Sites/ecomerce/xml/ecomerce.xml");
+$dom= simplexml_import_dom($xml);
+
+$numprodutos= $dom->count();
+
+
+
+for ($i=0; $i<$numprodutos; $i++){
+	
+	$dentro= $dom->produto[$i];
+	
+	$tipoproduto=$dentro->tipoproduto;
+	$nomeproduto=$dentro->nomeproduto;
+	$marcaproduto=$dentro->marcaproduto;
+	$descricaoproduto=$dentro->descricaoproduto;
+	$especificacoesproduto=$dentro->especificacoesproduto;
+	$precoproduto=$dentro->precoproduto;
+	$idproduto=$dentro->idproduto;
+	
+	
+
+	
+	
+}
+
+
+
+echo <<< _END
+<html>
+	$tipoproduto
+	</br>
+	$nomeproduto
+	</br>
+	$marcaproduto
+	</br>
+	$descricaoproduto
+	</br>
+	$especificacoesproduto
+	</br>
+	$precoproduto
+	</br>
+	$idproduto</html>
+_END;
+
+?>
